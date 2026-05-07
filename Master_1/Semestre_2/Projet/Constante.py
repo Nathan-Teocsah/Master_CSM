5bd9e87682fdd7dcba90cdd75842ee289fda0cea
petit-asus-linux <frelonjeu@gmail.com>
2026-05-07 18:08:14 +0200

    Projet_tutoré

---
+++ Master_1/Semestre_2/Projet/Constante.py
@@ -0,0 +1,174 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+T = 3.5e7 * 365.25 * 24 * 3600 # 35 millions d'années en secondes
+G = 6.67430e-11 # constante gravitationnelle
+M0 = 6.4185e23 # masse de Mars
+m = 1.06e16 # masse de Phobos
+R = 3396.2e3 # rayon de Mars (en mètres)
+k2 = 0.169 # nombre de Love de Mars : https://arxiv.org/html/2405.05519v1
+a0 = 9377e3 # demi-grand axe phobos (en mètres)
+omega_p = 2*np.pi/(24.622962*3600) # vitesse de rotation de Mars (rad/s)
+roche = 2.2*R # distance de Roche pour Phobos (en mètres) : https://www.insu.cnrs.fr/fr/cnrsinfo/phobos-la-lune-condamnee-pourquoi-mars-va-eroder-puis-disloquer-son-satellite
+alpha = 0.2 # Exposant de la loi de puissance
+
+def E(alpha) : # sec/rad (Q = E^alpha X^alpha)
+    match alpha :
+        case 0.2 : return  1201*10**5*24*3600
+        case 0.3 : return 81028*24*3600
+        case 0.4 : return 2104*24*3600
+
+def n(a) : # fréquence orbitale de Phobos (rad/s)  
+    return np.sqrt(G*(M0+m)/a**3)
+
+def X(a) : # fréquence de marée principale (s^(-1))
+    return 2*np.abs(omega_p - n(a))
+
+def Delta_t(a) : # lag temporel (s)
+    return E(alpha)**(-alpha) * X(a)**(-(alpha+1))
+
+def f1(a) : # dérivée du demi-grand axe (m/s)
+    numerateur = 6* k2 * R**5 * n(a) * m * Delta_t(a)
+    denominateur = M0 * a**4
+    val = -numerateur/denominateur * (n(a) - omega_p)
+    if type(a) == np.ndarray :
+        for i in range(len(a)) :
+            if a[i] < roche :
+                val[i] = 0
+        return val
+    else :
+        if a < roche :
+            val = 0
+        return val
+    
+
+def f(a) :
+    val = -C0 * a**(-5.5) * (C1*a**(-1.5) - omega_p)**(-alpha)
+    if type(a) == np.ndarray :
+        for i in range(len(a)) :
+            if a[i] < roche :
+                val[i] = 0
+        return val
+    if a < roche :
+        val = 0
+    return val
+    
+
+from scipy.integrate import solve_ivp
+
+def da_dt_for_solve_ivp(t, a):
+    return [f1(a[0])]  # solve_ivp attend un tableau
+
+def df_x(a) :
+    return B0 * a**(-6.5) * (C1*a**(-1.5) - omega_p)**(-alpha) - B1 * a**(-8) * (C1*a**(-1.5) - omega_p)**(-(alpha+1))
+
+
+def euler_explicite(a0, T, dt) :
+    t = np.linspace(0, T, int(T/dt)) # temps de 0 à T avec nb_point points
+    a = np.zeros(len(t)) # tableau pour stocker les valeurs de a
+    a[0] = a0
+    for i in range(1, len(t)) :
+        a[i] = a[i-1] + f1(a[i-1]) * dt # Euler explicite
+    return t, a
+
+
+import time
+
+start_time = time.time()
+sol_ref = solve_ivp(
+    da_dt_for_solve_ivp,
+    t_span=(0, T),
+    y0=[a0],
+    method='RK45',
+    rtol=1e-10,  # Tolérance relative très stricte
+    atol=1e-3,   # Tolérance absolue (1 mm)
+    dense_output=True
+)
+end_time = time.time()
+print("\n-----------------------------------------------")
+print(f"Temps de calcul pour RK45 : {(end_time - start_time)*1000:.0f} milisecondes.")
+
+
+# Résolution numérique de l'ODE
+dt = 10**2 # pas de temps en années
+dt = dt * 365.25 * 24 * 3600
+start_time = time.time()
+t, a = euler_explicite(a0, T, dt)
+end_time = time.time()
+print(f"Temps de calcul pour Euler explicite : {(end_time - start_time)*1000:.0f} milisecondes.")
+print("------------------------------------------")
+a_ref = sol_ref.sol(t)[0]
+
+
+
+# Calcul du coefficient de Lipschitz pour la fonction f sur [roche, a0]
+print("\nOrdre de grandeur des constantes : \n")
+C0 = 6*k2*R**5*m/M0 * E(alpha)**(-alpha) * np.sqrt(G*(M0+m)) * 2**(-(alpha+1))
+print(f"Constante C0 = {C0:E}")
+print("-----------------------------------------")
+
+C1 = np.sqrt(G*(M0+m))
+B0 = C0*5.5
+B1 = B0*alpha
+
+Intervalle_a = np.linspace(roche, a0, 10000)
+
+Lipschitz = np.max(np.abs(df_x(Intervalle_a)))
+print(f"Constante de Lipschitz L = {Lipschitz:E}")
+print("----------------------------------------")
+
+count = 0
+for i in range(len(Intervalle_a)) :
+    if n(Intervalle_a[i]) < omega_p :
+        count += 1
+if count > 0 :
+    print(f"---> !!!! Attention : n(a) < omega_p pour {count} valeurs de a dans l'intervalle [roche, a0].")
+
+M2 = np.max(np.abs(df_x(Intervalle_a)*f(Intervalle_a)))
+print(f"Constante M2_max = {M2:E}")
+M2 = np.max(np.abs(df_x(a_ref)*f(a_ref)))
+print(f"Constante M2_optim = {M2:E}")
+print("----------------------------------------")
+
+
+print(f"Temps de simulation T = {T:E} secondes.")
+print("----------------------------------------")
+
+C2 = np.exp(Lipschitz * T)*M2*T/2
+print(f"On a |y_n - y(t_n)| < {C2:E}*h")
+print("----------------------------------------")
+
+print(f"pour h = {dt:E} secondes :\n |y_n - y(t_n)| < {C2*dt:.2f} m.")
+
+print(f" sup |met_Euler - met_RK45| = {np.max(np.abs(a - a_ref)):.2f} m.")
+
+
+# Tracer de la courbe de f(a) pour alpha = 0.2
+plt.figure()
+plt.plot(Intervalle_a, f(Intervalle_a), label="f(a) pour alpha = 0.2")
+plt.xlabel("a")
+plt.ylabel("f(a)")
+plt.title("Fonction f(a) pour alpha = 0.2")
+plt.grid()
+plt.legend()
+
+# Tracer de la courbe f(a(t)) pour alpha = 0.2
+plt.figure()
+plt.plot(t/(365.25*24*3600e6), f(a_ref), label="f(a(t)) pour alpha = 0.2")
+plt.xlabel("Temps (millions d'années)")
+plt.ylabel("f(a(t))")
+plt.title("Fonction f(a(t)) pour alpha = 0.2")
+plt.grid()
+plt.legend()
+
+# Tracer de la courbe de f(a)f'(a) pour alpha = 0.2
+plt.figure()
+plt.plot(Intervalle_a, f(Intervalle_a)*df_x(Intervalle_a), label="f(a)f'(a) pour alpha = 0.2")
+plt.xlabel("a")
+plt.ylabel("f(a)f'(a)")
+plt.title("Produit de f(a) et f'(a) pour alpha = 0.2")
+plt.grid()
+plt.legend()
+
+
+plt.show()
--- Master_1/Semestre_2/Projet/Semi-axe_Euler.py
+++ Master_1/Semestre_2/Projet/Semi-axe_Euler.py
@@ -5,7 +5,7 @@ G = 6.67430e-11 # constante gravitationnelle
 M0 = 6.4185e23 # masse de Mars
 m = 1.06e16 # masse de Phobos
 R = 3396.2e3 # rayon de Mars (en mètres)
-k2 = 0.15 # nombre de Love de Mars (A revoir)
+k2 = 0.169 # nombre de Love de Mars : https://arxiv.org/html/2405.05519v1
 a0 = 9377e3 # demi-grand axe phobos (en mètres)
 omega_p = 2*np.pi/(24.622962*3600) # vitesse de rotation de Mars (rad/s)
 roche = 2.2*R # distance de Roche pour Phobos (en mètres) : https://www.insu.cnrs.fr/fr/cnrsinfo/phobos-la-lune-condamnee-pourquoi-mars-va-eroder-puis-disloquer-son-satellite
@@ -34,7 +34,7 @@ def da_dt(a, alpha) : # dérivée du demi-grand axe (m/s)
     return -numerateur/denominateur * (n(a) - omega_p)
 
 def euler_explicite(a0, alpha, T, dt) :
-    t = np.linspace(0, T, int(T/dt)) # temps de 0 à T avec nb_point points
+    t = np.arange(0, T, dt) # temps de 0 à T avec nb_point points
     a = np.zeros(len(t)) # tableau pour stocker les valeurs de a
     a[0] = a0
     for i in range(1, len(t)) :
@@ -42,7 +42,7 @@ def euler_explicite(a0, alpha, T, dt) :
     return t, a
 
 # Résolution numérique de l'ODE
-T = 3.5e7 * 365.25 * 24 * 3600 # 50 millions d'années en secondes
+T = 3.5*10**7 * 365.25 * 24 * 3600 # 50 millions d'années en secondes
 index = 0 # index pour alpha = 0.2
 Alpha = alpha[index]
 dt = 10**2 # pas de temps en années
@@ -50,7 +50,10 @@ dt = dt * 365.25 * 24 * 3600 # conversion du pas de temps en secondes
 print(f"Pas de temps : {dt:E} secondes.")
 print(f"Pas de temps : {dt/(3600*24*365.25):E} années.")
 print(f"Nombre de points : {int(T/dt):E}.")
+
 t,a = euler_explicite(a0, Alpha, T, dt)
+
+
 
 
 Delta = np.zeros(len(t)) # tableau pour stocker les valeurs de Delta_t
@@ -61,10 +64,6 @@ for i in range(1, len(t)) :
     if a[i] <= roche and zero == -1 : 
         zero = i
 
-print(f"Phobos atteint la Roche après {t[zero]/(365.25*24*3600e6):.4f} millions d'années.")
-
-
-print("Runge-Kutta 4ème ordre :")
 from scipy.integrate import solve_ivp
 
 def da_dt_for_solve_ivp(t, a):
@@ -75,19 +74,24 @@ sol_ref = solve_ivp(
     t_span=(0, T),
     y0=[a0],
     method='RK45',
-    rtol=1e-10,  # Tolérance relative très stricte
-    atol=1e-3,   # Tolérance absolue (1 mm)
+    rtol=1e-14,  # Tolérance relative très stricte
+    atol=1e-6,   # Tolérance absolue (1 mm)
     dense_output=True
 )
 
-t_ref = sol_ref.t
-a_ref = sol_ref.y[0]
+a_ref = sol_ref.sol(t)[0]
 zero_ref = np.argmax(a_ref < roche)
-print(f"Phobos atteint la Roche après {t_ref[zero_ref]/(365.25*24*3600e6):.4f} millions d'années (référence RK45).")
+print(f"Phobos atteint la Roche après {t[zero]/(365.25*24*3600e6):.6f} millions d'années.")
+print(f"Phobos atteint la Roche après {t[zero_ref]/(365.25*24*3600e6):.6f} millions d'années (référence RK45).")
+print(f"----> Différence entre les deux méthodes : {(t[zero] - t[zero_ref])/(365.25*24*3600*10**6):.6f} millions d'années.")
+print(f"sup |met_Euler - met_RK45| = {np.max(np.abs(a - a_ref)):.6f} mètres.")
+
+
+
 
 plt.figure()
-plt.plot(t_ref/(365.25*24*3600e6), a_ref/1e3, label="Chute de a (km) pour alpha = 0.2")
-plt.plot(t/(365.25*24*3600e6), a/1e3,color='red', label="Chute de a (km) pour alpha = 0.2")
+plt.plot(t/(365.25*24*3600 * 10**6), a_ref*10**(-3), label="Chute de a (km) pour alpha = 0.2")
+plt.plot(t/(365.25*24*3600 * 10**6), a*10**(-3), color='red', label="Chute de a (km) pour alpha = 0.2")
 plt.xlabel("Temps (millilons d'années)")
 plt.ylabel("Demi-grand axe (km)")
 plt.title("Évolution du demi-grand axe de Phobos")
@@ -95,8 +99,8 @@ plt.grid()
 plt.legend()
 
 plt.figure()
-plt.plot(t_ref/(365.25*24*3600e6), Delta_t(a_ref, Alpha)/60, label="Lag temporel (en min) pour alpha = 0.2")
-plt.plot(t/(365.25*24*3600e6), Delta/60, color='red', label="Lag temporel (en min) pour alpha = 0.2")
+plt.plot(t/(365.25*24*3600 * 10**6), Delta_t(a_ref, Alpha)/60, label="Lag temporel (en min) pour alpha = 0.2")
+plt.plot(t/(365.25*24*3600 * 10**6), Delta/60, color='red', label="Lag temporel (en min) pour alpha = 0.2")
 plt.xlabel("Temps (millilons d'années)")
 plt.ylabel("Delta_t (min)")
 plt.title("Évolution du lag temporel de Phobos")
@@ -105,34 +109,67 @@ plt.legend()
 
 
 # Calcul de l'erreur 
-
+print("\n Calcul de l'erreur pour différents pas")
 Nb_point = 10**3 # nombre de points pour l'erreur
-dt0 = 10**2  # pas de temps (en années) pour l'erreur
+dt0 = 10**2  # pas de temps initial pour l'erreur (en secondes)
+dt_fin = 10**5
 
-Erreur = np.zeros(Nb_point-1) # tableau pour stocker les erreurs
-DT = np.zeros(Nb_point-1) # tableau pour stocker les pas de temps
+dt_fin = dt_fin * 365.25 * 24 * 3600 # conversion du pas de temps final en secondes
+dt0 = dt0 * 365.25 * 24 * 3600 # conversion du pas de temps initial en secondes
+DT = np.linspace(dt0, dt_fin, Nb_point)  # pas de temps (en années) pour l'erreur
 
-dt0 = dt0 * 365.25 * 24 * 3600 # conversion du pas de temps en secondes
+Erreur = np.zeros(Nb_point) # tableau pour stocker les erreurs
 
-x_query = np.linspace(0, T, 10**2) # temps de 0 à T avec nb_point points
-func_ref = np.interp(x_query, t_ref, a_ref) # interpolation de la solution de référence sur la grille commune
-for i in range(2,Nb_point+1) :
-    dt = i * dt0 # pas de temps qui varie de 5e8 à 5e11
-    DT[i-2] = dt
-    #print(f"\nCalcul de l'erreur pour dt = {dt/(365.25*24*3600):E} années.")
-    #print(f"progression : {(i+1)/Nb_point*100:.2f} %")
+import sys
+import time
+temps = np.zeros(Nb_point)
+temps_total=time.time()
+for i in range(Nb_point) :
+    dt = DT[i]
+    print(f"\rprogression : {(i+1)/Nb_point*100:.2f} %", end='', file=sys.stdout)
+    temps[i] = time.time()
     t,a = euler_explicite(a0, Alpha, T, dt)
-    func = np.interp(x_query, t, a, left=np.nan, right=np.nan)
-    Erreur[i-2] = np.max(np.abs(func - func_ref))
+    temps[i] = time.time() - temps[i]
+    ind_roche = np.min([np.argmax(a <= roche),np.argmax(sol_ref.sol(t)[0] <= roche)])
+    Erreur[i] = np.max(np.abs(a[0:ind_roche] - sol_ref.sol(t)[0][0:ind_roche]))
+    Erreur[i] = Erreur[i]/np.max(np.abs(sol_ref.sol(t)[0][0:ind_roche]))
+
+print(f"\nTemps total = {time.time()-temps_total:.2f} s.")
+
+print("\n\n Régression linéaire pour trouver l'ordre de convergence :")
+from sklearn.linear_model import LinearRegression
+
+# Sample data
+x = np.log(DT).reshape(-1, 1)  # Reshape for scikit-learn
+y = np.log(Erreur)
+
+# Create model instance
+model = LinearRegression()
+
+# Fit the model
+model.fit(x, y)
+
+# Get model parameters
+print(f"---> Ordre de convergence : {model.coef_[0]}")
+print(f"Estimation de C : |y_n-y(t_n)| <= C*h^{model.coef_[0]}")
+print(f"---> C = exp({model.intercept_}) = {np.exp(model.intercept_):E}")
 
 plt.figure()
-plt.plot(DT/(365.25*24*3600), Erreur, '-+', label="Erreur relative en fonction du pas de temps")
+plt.plot(DT/(365.25*24*3600), Erreur)
 plt.xlabel("pas de temps (années)")
 plt.ylabel("Erreur (en km)")
-plt.title("Erreur (en norme infinie) en fonction du pas de temps")
+plt.title("Erreur (en norme infinie) pour la méthode d'Euler")
 plt.xscale("log")
 plt.yscale("log")
 plt.grid()
-plt.legend()
+
+plt.figure()
+plt.plot(DT/(365.25*24*3600), temps)
+plt.xlabel("pas de temps (années)")
+plt.ylabel("Temps (s.)")
+plt.title("Temps d'execution de la méthode de Euler")
+plt.xscale("log")
+plt.yscale("log")
+plt.grid()
 
 plt.show()
